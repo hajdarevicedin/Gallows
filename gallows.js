56f77598320283275fbd8d7ef737aa2395dbf52b
Edin Hajdarevic <hajdarevicedin11@gmail.com>
2019-11-21 14:07:34 +0100

    updated most of game, even I dont know what

--- gallows.js
+++ gallows.js
@@ -1,20 +1,28 @@
 var word1;
-
+var word2;
 function getTheWord(){
     var neededWord = document.getElementById("word-input1").value;
     
+	word1 = neededWord;
     document.getElementById("word-input1").value = "";
-
-    word1 = neededWord;
+	
+	wordlen = word1.length;
+	for(let ii=0; ii<wordlen; ii++) {
+		document.getElementById("wordfinder").innerHTML = document.getElementById("wordfinder").innerHTML + "*";	
+	}
+	
+	word2 = document.getElementById("wordfinder").innerHTML;
 }
 
 function submitTheLetter() {
     var letter = document.getElementById("letter-input1").value;
-
-    let i=0;
+	//var word2 = document.getElementById("wordfinder").innerHTML;
+    var i=0;
     for(i in word1) {
         if(word1[i] == letter) {
-            console.log("SUCCESS");
-        } else console.log("FAIL");
+            word2[i] = letter;
+        }
     }
+	
+	document.getElementById("wordfinder").innerHTML = word2;
 }
